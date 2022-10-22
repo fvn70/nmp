6c5bfc28151db863e0feafdb790bc01a0bb7bb02
fvn70 <fvn_reg@mail.ru>
2022-10-22 09:45:22 +0700

    Stage 3/6: Matrix by matrix multiplication

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,11 +1,28 @@
+from array import array
+
 import numpy as np
 
-def read_matrix():
-    n, m = map(int, input().split(' '))
-    arr = np.empty((n, m), dtype=int)
+MENU = """
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+0. Exit
+"""
+
+def read_matrix(num):
+    txt = 'first' if num == 1 else 'second' if num == 2 else ''
+    msg = f"Enter size of {txt} matrix: "
+    n, m = map(int, input(msg).split(' '))
+    print(f'Enter {txt} matrix:')
+    # arr = np.empty((n, m), dtype=float)
+    lst = []
     for i in range(n):
-        s = input()
-        arr[i] = [int(v) for v in s.split(' ')]
+        lst.append(input().split(' '))
+        # arr[i] = [float(v) for v in s.split(' ')]
+    if '.' in str(lst):
+        arr = np.array(lst, dtype=float)
+    else:
+        arr = np.array(lst, dtype=int)
     return arr
 
 def print_matrix(arr):
@@ -15,22 +32,56 @@ def print_matrix(arr):
 
 def add_matrix(arr1, arr2):
     if arr1.shape == arr2.shape:
+        print('The result is:')
         print_matrix(arr1 + arr2)
     else:
-        print('ERROR')
+        print('The operation cannot be performed')
 
-def mul_matrix(arr, k):
+def mul_matrix(arr1, arr2):
+    if arr1.shape[1] == arr2.shape[0]:
+        print('The result is:')
+        print_matrix(arr1 @ arr2)
+    else:
+        print('The operation cannot be performed')
+
+def k_matrix(arr, k):
     return k * arr
 
-def stage1():
-    arr1 = read_matrix()
-    arr2 = read_matrix()
-    arr = arr1 + arr2
-    print_matrix(arr)
+def do_add():
+    arr1 = read_matrix(1)
+    arr2 = read_matrix(2)
+    add_matrix(arr1, arr2)
 
-def stage2():
-    arr = read_matrix()
-    k = int(input())
-    print_matrix(mul_matrix(arr, k))
+def do_mul_2():
+    arr1 = read_matrix(1)
+    arr2 = read_matrix(2)
+    mul_matrix(arr1, arr2)
 
-stage2()
+def do_mul_k():
+    arr = read_matrix(0)
+    k = input('Enter constant: ')
+    if '.' in k:
+        k = float(k)
+    else:
+        k = int(k)
+    print('The result is:')
+    print_matrix(k_matrix(arr, k))
+
+def menu():
+    while True:
+        print(MENU)
+        cmd = int(input('Your choice: '))
+        if cmd == 0:
+            break
+        elif cmd == 1:
+            do_add()
+        elif cmd == 2:
+            do_mul_k()
+        elif cmd == 3:
+            do_mul_2()
+        else:
+            print('Wrong choice!')
+    return
+
+
+menu()
